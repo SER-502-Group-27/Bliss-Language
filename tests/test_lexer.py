b2566971db6cf041f34a18b66c9d21184d48b8e9
Ansh <aagra109@asu.edu>
2024-04-25 18:51:57 -0700

    test: added test cases for arithmetic operators

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -35,6 +35,51 @@ class TestLexer(unittest.TestCase):
         self.assertEqual(token.type, "STRING_LITERAL")
         self.assertEqual(token.value, "hello world")
 
+    def test_boolean_true(self):
+        self.lexer.input("True")
+        token = self.lexer.token()
+        self.assertIsNotNone(token)
+        self.assertEqual(token.type, "BOOLEAN")
+        self.assertEqual(token.value, True)
+
+    def test_boolean_false(self):
+        self.lexer.input("False")
+        token = self.lexer.token()
+        self.assertIsNotNone(token)
+        self.assertEqual(token.type, "BOOLEAN")
+        self.assertEqual(token.value, False)
+
+    def test_plus_operator(self):
+        self.lexer.input("+")
+        token = self.lexer.token()
+        self.assertIsNotNone(token)
+        self.assertEqual(token.type, "PLUS")
+
+    def test_minus_operator(self):
+        self.lexer.input("-")
+        token = self.lexer.token()
+        self.assertIsNotNone(token)
+        self.assertEqual(token.type, "MINUS")
+
+    def test_times_operator(self):
+        self.lexer.input("*")
+        token = self.lexer.token()
+        self.assertIsNotNone(token)
+        self.assertEqual(token.type, "TIMES")
+
+    def test_divide_operator(self):
+        self.lexer.input("/")
+        token = self.lexer.token()
+        self.assertIsNotNone(token)
+        self.assertEqual(token.type, "DIVIDE")
+
+    def test_modulo_operator(self):
+        self.lexer.input("%")
+        token = self.lexer.token()
+        self.assertIsNotNone(token)
+        self.assertEqual(token.type, "MODULO")
+
+
 
 if __name__ == "__main__":
     unittest.main()
